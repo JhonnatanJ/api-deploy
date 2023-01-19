ad236635646760b6dc35fa936c1b661d4b73ac78
Jhonnatan Bonilla <javiermarquez_99@hotmail.com>
2023-01-18 19:57:26 -0500

    Agregar favico y permisos en authorizeRequests

--- src/main/java/com/lib/api/auth/ResourceServerConfig.java
+++ src/main/java/com/lib/api/auth/ResourceServerConfig.java
@@ -22,6 +22,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers( "/favicon.ico").permitAll()
                 .antMatchers(HttpMethod.GET, "/geolib/libros/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/geolib/imagenes/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/geolib/imagenes/**").permitAll()
@@ -32,6 +33,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().cors().configurationSource(corsConfigurationSource());
     }
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
